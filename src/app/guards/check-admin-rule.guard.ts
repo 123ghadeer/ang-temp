689e6d4b46df0dcec1f522a0f578df4a174bd57f
123ghadeer <ghadeerghazimig@gmail.com>
2021-12-23 21:44:38 +0200

    save changes

--- src/app/guards/check-admin-rule.guard.ts
+++ src/app/guards/check-admin-rule.guard.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class CheckPermissionGuard implements CanActivate {
-  constructor(
-  ) {
-  }
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class CheckPermissionGuard implements CanActivate {
+//   constructor(
+//   ) {
+//   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-      const authData = JS
-    return this.securityService.hasSuperAccess();
-  }
-}
+//   // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+//   //     const authData = JS
+//   //   return this.securityService.hasSuperAccess();
+//   // }
+// }
